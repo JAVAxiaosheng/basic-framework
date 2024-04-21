260093af991c2c942e5ef01d7275a9a7e747ac04
unknown <luzhan@bilibili.com>
2024-04-21 22:02:22 +0800

    commit

--- basic-server/src/main/java/com/base/frame/BasicFrameworkApplication.java
+++ basic-server/src/main/java/com/base/frame/BasicFrameworkApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.base")
 public class BasicFrameworkApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BasicFrameworkApplication.class, args);
     }
-
 }
