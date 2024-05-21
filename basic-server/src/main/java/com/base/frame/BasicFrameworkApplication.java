64f51e4d4d5891f305693193987e35cadc2399af
unknown <luzhan@bilibili.com>
2024-05-21 10:42:51 +0800

    commit

--- basic-server/src/main/java/com/base/frame/BasicFrameworkApplication.java
+++ basic-server/src/main/java/com/base/frame/BasicFrameworkApplication.java
@@ -2,8 +2,10 @@ package com.base.frame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.base")
+@EnableDiscoveryClient
 public class BasicFrameworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(BasicFrameworkApplication.class, args);
